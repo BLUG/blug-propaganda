cec95517db83e65d3d23a26860699bd02cec6ca8
Bård Aase <elzapp@gmail.com>
2016-01-18 14:06:14 +0100

    la inn mer spesifik info om miniforedrag

--- Planlegging.mm
+++ Planlegging.mm
@@ -2,7 +2,33 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1453121779361" ID="ID_1138745441" MODIFIED="1453121793149" TEXT="BLUG Planlegging">
 <node CREATED="1453121838513" ID="ID_937094852" MODIFIED="1453121854738" POSITION="right" TEXT="2016">
-<node CREATED="1453121858543" ID="ID_774597501" MODIFIED="1453121897240" TEXT="Januar: Miniforedrag"/>
+<node CREATED="1453121858543" ID="ID_774597501" MODIFIED="1453122324795" TEXT="Januar: Miniforedrag">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Let's Encrypt - TLS for alle - B&#229;rd Aase<br />
+      </li>
+      <li>
+        Hva er nytt i ECMAScript 6? - Hans Kristian Flaatten
+      </li>
+      <li>
+        Spar penger og forbedre sikkerheten p&#229; din webserver ved &#229; heve sikkerheten! - Per Thorsheim
+      </li>
+      <li>
+        Ikke alle brukere kan erstattes med et lite shell-script - L. Cecilie Wian
+      </li>
+      <li>
+        Verkt&#248;ykasse for &#229; lage fysiske 3d-modeller - Gisle S&#230;lensminde
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
 <node CREATED="1453121861236" ID="ID_143439406" MODIFIED="1453121910572" TEXT="Februar: Smarte hjem"/>
 <node CREATED="1453121864955" ID="ID_598856353" MODIFIED="1453121918342" TEXT="Mars: &#xc5;pne data"/>
 <node CREATED="1453121868287" ID="ID_1805595166" MODIFIED="1453121934164" TEXT="April: Open Source i bedrifter"/>
